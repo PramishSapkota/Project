3dae899abd73bd9e4a40edaf7eb4176c81c5fbbb
Utsav Poudel <utsavpoudel057@gmail.com>
2025-02-01 09:03:58 +0545

    Flask

---
+++ Minor-Project-main/Login Page/static/script.js
@@ -0,0 +1,31 @@
+// Wait for the page to load and then apply the 'loaded' class to fade in
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+});
+
+// const container = document.querySelector('.container');
+// const registerBtn = document.querySelector('.register-btn');
+// const loginBtn = document.querySelector('.login-btn');
+
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add('active');
+// });
+
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove('active');
+// });
+
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".container");
+    const registerBtn = document.querySelector(".register-btn");
+    const loginBtn = document.querySelector(".login-btn");
+
+    registerBtn.addEventListener("click", () => {
+        container.classList.add("active");
+    });
+
+    loginBtn.addEventListener("click", () => {
+        container.classList.remove("active");
+    });
+});
